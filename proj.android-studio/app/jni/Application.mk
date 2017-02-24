499a9ba9f1d8b641ddaf6a3ffe0fd2c35bae6e42
Wong Yong Jie <yjwong92@gmail.com>
2017-02-24 03:43:01 -0500

    Target API 19 on NDK (fixes crash on KitKat).

--- proj.android-studio/app/jni/Application.mk
+++ proj.android-studio/app/jni/Application.mk
@@ -3,8 +3,8 @@ APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
 
-APP_ABI := armeabi
-
+APP_ABI := x86 armeabi
+APP_PLATFORM := android-19
 
 ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
