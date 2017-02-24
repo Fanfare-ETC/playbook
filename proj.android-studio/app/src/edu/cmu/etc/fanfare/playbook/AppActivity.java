6eae5788647d2d5b4df3f93d2e9a6f532b092856
Wong Yong Jie <yjwong92@gmail.com>
2017-02-24 02:58:42 -0500

    Iconified the menu.

--- proj.android-studio/app/src/edu/cmu/etc/fanfare/playbook/AppActivity.java
+++ proj.android-studio/app/src/edu/cmu/etc/fanfare/playbook/AppActivity.java
@@ -23,20 +23,34 @@ THE SOFTWARE.
 ****************************************************************************/
 package edu.cmu.etc.fanfare.playbook;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 public class AppActivity extends AppCompatActivity {
-    private String[] mMenuItems;
+    private static final String TAG = "AppActivity";
+
+    private DrawerItem[] mMenuItems;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
@@ -47,6 +61,46 @@ public class AppActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             selectItem(position);
+        }
+    }
+
+    private class DrawerItem {
+        String text;
+        Drawable icon;
+    }
+
+    private class DrawerItemAdapter extends ArrayAdapter<DrawerItem> {
+        private ViewHolder mHolder;
+
+        DrawerItemAdapter(Context context, int resource, DrawerItem[] objects) {
+            super(context, resource, objects);
+        }
+
+        private class ViewHolder {
+            private TextView mTextView;
+        }
+
+        @NonNull
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(AppActivity.this)
+                    .inflate(R.layout.drawer_list_item, parent, false);
+
+                mHolder = new ViewHolder();
+                mHolder.mTextView = (TextView) convertView.findViewById(R.id.text);
+
+                convertView.setTag(mHolder);
+            } else {
+                mHolder = (ViewHolder) convertView.getTag();
+            }
+
+            DrawerItem item = getItem(position);
+            if (item != null) {
+                mHolder.mTextView.setText(item.text);
+                mHolder.mTextView.setCompoundDrawablesRelativeWithIntrinsicBounds(null, item.icon, null, null);
+            }
+
+            return convertView;
         }
     }
 
@@ -73,6 +127,22 @@ public class AppActivity extends AppCompatActivity {
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 
+    private DrawerItem[] getDrawerItems() {
+        String[] menuItems = getResources().getStringArray(R.array.menu_items);
+        TypedArray menuItemIcons = getResources().obtainTypedArray(R.array.menu_item_icons);
+        DrawerItem[] drawerItems = new DrawerItem[menuItems.length];
+
+        for (int i = 0; i < menuItems.length; i++) {
+            DrawerItem drawerItem = new DrawerItem();
+            drawerItem.text = menuItems[i];
+            drawerItem.icon = menuItemIcons.getDrawable(i);
+            drawerItems[i] = drawerItem;
+        }
+
+        menuItemIcons.recycle();
+        return drawerItems;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,7 +152,7 @@ public class AppActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
 
         mTitle = mDrawerTitle = getTitle();
-        mMenuItems = getResources().getStringArray(R.array.menu_items);
+        mMenuItems = getDrawerItems();
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, myToolbar,
@@ -105,7 +175,7 @@ public class AppActivity extends AppCompatActivity {
 
         // Setup the menu items
         // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<>(this,
+        mDrawerList.setAdapter(new DrawerItemAdapter(this,
                 R.layout.drawer_list_item, mMenuItems));
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
