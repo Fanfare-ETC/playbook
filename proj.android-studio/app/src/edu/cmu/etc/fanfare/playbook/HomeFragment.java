39093893861074d7e3adae5a21c14dc6c4ca853f
Wong Yong Jie <yjwong92@gmail.com>
2017-02-28 23:10:31 -0500

    Added seat selection fragment/activity.

--- proj.android-studio/app/src/edu/cmu/etc/fanfare/playbook/HomeFragment.java
+++ proj.android-studio/app/src/edu/cmu/etc/fanfare/playbook/HomeFragment.java
@@ -1,5 +1,6 @@
 package edu.cmu.etc.fanfare.playbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,6 +12,18 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        // Open stadium selection activity.
+        View stadiumSelectNotification = view.findViewById(R.id.stadium_select_notification);
+        stadiumSelectNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SeatSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
