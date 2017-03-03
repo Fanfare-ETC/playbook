1e3326aa1ed3d834c98c40182ceca23dccbdfe11
Wong Yong Jie <yjwong92@gmail.com>
2017-03-03 03:36:07 -0500

    Use libc++ so we can use std::to_string.

--- proj.android-studio/app/jni/Application.mk
+++ proj.android-studio/app/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_STL := gnustl_static
+APP_STL := c++_static
 
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
@@ -7,7 +7,7 @@ APP_ABI := x86 armeabi
 APP_PLATFORM := android-19
 
 ifeq ($(NDK_DEBUG),1)
-  APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
+  APP_CPPFLAGS += -DCOCOS2D_DEBUG=1 -DCC_NODE_DEBUG_VERIFY_EVENT_LISTENERS=1
   APP_OPTIM := debug
 else
   APP_CPPFLAGS += -DNDEBUG
