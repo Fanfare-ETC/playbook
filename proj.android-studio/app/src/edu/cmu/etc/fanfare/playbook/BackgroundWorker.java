8b03adfbe72fc166c6e38f65cab93df704c5e26e
ZoeQi <qiyiru@gmail.com>
2017-03-13 13:49:02 -0400

    section selection

--- proj.android-studio/app/src/edu/cmu/etc/fanfare/playbook/BackgroundWorker.java
+++ proj.android-studio/app/src/edu/cmu/etc/fanfare/playbook/BackgroundWorker.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -83,6 +86,7 @@ public class  BackgroundWorker extends AsyncTask<String,Void,String> {
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 outputStream.close();
+
                 InputStream inputStream = httpURLConnection.getInputStream();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
                 String result="";
@@ -130,6 +134,48 @@ public class  BackgroundWorker extends AsyncTask<String,Void,String> {
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (params[0].equals("section")) {
+            try {
+                String sec=Integer.toString(section);
+                Log.v("sec",Integer.toString(section));
+                //String move = "2";
+                URL url = new URL("http://10.0.2.2:8080/events");
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setRequestProperty("Accept", "application/json");
+                httpURLConnection.setRequestProperty("Content-type", "application/json");
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                JSONObject object = new JSONObject();
+                object.put("id", section);
+                Log.d("test", object.toString());
+               /* String post_data = URLEncoder.encode("sectionNo","UTF-8")+"="+URLEncoder.encode(sec,"UTF-8")+"&"
+                        + URLEncoder.encode("Move","UTF-8")+"="+URLEncoder.encode(move,"UTF-8");*/
+                bufferedWriter.write(object.toString());
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                Log.d("HTTP", "Response code: " + httpURLConnection.getResponseCode());
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
--- proj.android-studio/app/src/edu/cmu/etc/fanfare/playbook/SeatSelectFragment.java
+++ proj.android-studio/app/src/edu/cmu/etc/fanfare/playbook/SeatSelectFragment.java
@@ -1,35 +1,162 @@
 package edu.cmu.etc.fanfare.playbook;
 
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
-public class SeatSelectFragment extends Fragment  {
-    public static int section;
+
+public class SeatSelectFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    public static int section = 0;
+    private ImageView mStadiumMap;
+    private ImageView arrow;
+
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        final View view=inflater.inflate(R.layout.stadium_select_activity, container, false);
+        final View view=inflater.inflate(R.layout.stadium_select_fragment, container, false);
 
-        //create a list box to enter section
+        mStadiumMap = (ImageView) view.findViewById(R.id.stadium_map);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.select_dialog_singlechoice);
-        arrayAdapter.add("1");
-        arrayAdapter.add("2");
-        arrayAdapter.add("3");
+        final Spinner spinner = (Spinner)view.findViewById(R.id.sections_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.sections_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        arrow = (ImageView) view.findViewById(R.id.btn_dropdown);
+        arrow.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                spinner.performClick();
+            }
+        });
+
+        Button button = (Button) view.findViewById(R.id.proceed);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BackgroundWorker backgroundWorker = new BackgroundWorker(section);
+                backgroundWorker.execute("section");
+
+                /*
+                Fragment predictionFragment = new PredictionFragment();
+
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment_container, predictionFragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack(null);
+                ft.commit();
+                */
+                Intent intent = new Intent(v.getContext(), AppActivity.class);
+                Bundle extras = new Bundle();
+
+                extras.putString("EXTRA_MESSAGE", Integer.toString(section));
+
+                intent.putExtras(extras);
+                //intent.putExtra(EXTRA_MESSAGE, seatNo);
+                //intent.putExtra(EXTRA_COLOR, colorController);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        Button button1 = (Button) view.findViewById(R.id.section1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Spinner spinner = (Spinner) v.findViewById(R.id.sections_spinner);
+                spinner.setSelection(0);
+
+
+
+            }
+        });
+        Button button2 = (Button) view.findViewById(R.id.section2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Spinner spinner = (Spinner) v.findViewById(R.id.sections_spinner);
+                spinner.setSelection(1);
+
+
+
+            }
+        });
+        Button button3 = (Button) view.findViewById(R.id.section3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Spinner spinner = (Spinner) v.findViewById(R.id.sections_spinner);
+                spinner.setSelection(2);
+
+
+
+            }
+        });
+        Button button4 = (Button) view.findViewById(R.id.section4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Spinner spinner = (Spinner) v.findViewById(R.id.sections_spinner);
+                spinner.setSelection(3);
+
+
+
+            }
+        });
+
 
         return view;
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        Log.d("Test","Selected:"+ parent.getItemAtPosition(pos).toString());
+         switch (parent.getItemAtPosition(pos).toString()) {
+             case "Section 1":
+                 section = 1;
+                 mStadiumMap.setImageResource(R.drawable.stadium_map_1);
+                 break;
+             case "Section 2":
+                 section = 2;
+                 mStadiumMap.setImageResource(R.drawable.stadium_map_2);
+                 break;
+             case "Section 3":
+                 section = 3;
+                 mStadiumMap.setImageResource(R.drawable.stadium_map_3);
+                 break;
+             case "Section 4":
+                 section = 4;
+                 mStadiumMap.setImageResource(R.drawable.stadium_map_4);
+                 break;
+         }
+
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+
+        Log.d("Test", "Nothing Selected");
+    }
+
 
 
 
