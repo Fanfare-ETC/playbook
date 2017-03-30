c98fa68759f338986caf2400ddc9f590c1a1a953
Wong Yong Jie <yjwong92@gmail.com>
2017-03-30 17:46:43 -0400

    Functional blur filter.

--- Classes/BlurFilter.h
+++ Classes/BlurFilter.h
@@ -9,9 +9,12 @@ public:
 
 private:
     static const std::map<int, std::vector<float>> GAUSSIAN_VALUES;
+    enum Direction { X, Y };
 
     int getMaxKernelSize();
+    cocos2d::RenderTexture* apply(cocos2d::RenderTexture* input, int inputStrength, Direction direction);
     cocos2d::RenderTexture* applyX(cocos2d::RenderTexture* input, int inputStrength);
+    cocos2d::RenderTexture* applyY(cocos2d::RenderTexture* input, int inputStrength);
     std::string generateVertBlurSource(int kernelSize, bool x);
     std::string generateFragBlurSource(int kernelSize);
 };
