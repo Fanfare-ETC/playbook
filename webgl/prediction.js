78011bb158f2a064433feaf8a928d367a3346ab7
Wong Yong Jie <yjwong92@gmail.com>
2017-04-27 03:13:38 -0400

    Changed prediction overlay to use Rockwell font.

--- webgl/prediction.js
+++ webgl/prediction.js
@@ -428,14 +428,12 @@ class PredictionCorrectOverlay extends PIXI.Container {
   constructor(event, addedScore) {
     super();
 
-    /** @type {PIXI.Graphics} */
     this.background = new PIXI.Graphics();
     this.background.beginFill(0x000000, 0.75);
     this.background.drawRect(0, 0, window.innerWidth, window.innerHeight);
     this.background.endFill();
     this.addChild(this.background);
 
-    /** @type {PIXI.Sprite} */
     this.ball = new PIXI.Sprite(PIXI.loader.resources['resources/Item-Ball-Rotated.png'].texture);
     const ballScale = window.innerWidth / this.ball.texture.width;
     this.ball.scale.set(ballScale, ballScale);
@@ -443,16 +441,61 @@ class PredictionCorrectOverlay extends PIXI.Container {
     this.ball.anchor.set(0.5, 0.5);
     this.addChild(this.ball);
 
-    /** @type {PIXI.Text} */
+    const textStyle = new PIXI.TextStyle({
+      fontFamily: 'rockwell',
+      fontWeight: 'bold',
+      fontSize: 72.0 * contentScale,
+      align: 'center',
+      fill: 0x002b65
+    });
+
+    this.textContainer = new PIXI.Container();
+
     this.text = new PIXI.Text();
-    this.text.position.set(window.innerWidth / 2, window.innerHeight / 2);
-    this.text.anchor.set(0.5, 0.5);
-    this.text.text = `Prediction correct:\n ${PlaybookEventsFriendlyNames[event]}\n\nYou got ${addedScore} ${addedScore > 1 ? 'points' : 'point'}!`;
-    this.text.style.fontFamily = 'proxima-nova';
-    this.text.style.fontWeight = 'bold';
-    this.text.style.fontSize = 24.0;
-    this.text.style.align = 'center';
-    this.addChild(this.text);
+    this.text.text = `Prediction Correct:\n ${PlaybookEventsFriendlyNames[event]}\n`;
+    this.text.style = textStyle;
+    const textMetrics = PIXI.TextMetrics.measureText(this.text.text, this.text.style);
+    this.textContainer.addChild(this.text);
+
+    this.scoreContainer = new PIXI.Container();
+
+    this.score1 = new PIXI.Text();
+    this.score1.text = 'You got ';
+    this.score1.style = textStyle;
+    this.score1.position.set(0, textMetrics.height);
+    const textMetricsScore1 = PIXI.TextMetrics.measureText(this.score1.text, this.score1.style);
+    this.scoreContainer.addChild(this.score1);
+
+    this.score2 = new PIXI.Text();
+    this.score2.text = addedScore.toString();
+    this.score2.style.fontFamily = 'SCOREBOARD';
+    this.score2.style.fontSize = 72.0 * contentScale;
+    this.score2.style.align = 'center';
+    const textMetricsScore2 = PIXI.TextMetrics.measureText(this.score2.text, this.score2.style);
+    this.score2.position.set(
+      textMetricsScore1.width,
+      textMetrics.height + (textMetricsScore1.height - textMetricsScore2.height - textMetrics.fontProperties.descent)
+    );
+    this.scoreContainer.addChild(this.score2);
+
+    this.score3 = new PIXI.Text();
+    this.score3.text = ` ${addedScore > 1 ? 'points' : 'point'}!`;
+    this.score3.style = textStyle;
+    this.score3.position.set(textMetricsScore1.width + textMetricsScore2.width, textMetrics.height);
+    const textMetricsScore3 = PIXI.TextMetrics.measureText(this.score3.text, this.score3.style);
+    this.scoreContainer.addChild(this.score3);
+
+    this.textContainer.addChild(this.scoreContainer);
+    this.textContainer.position.set(
+      window.innerWidth / 2 - this.textContainer.width / 2,
+      window.innerHeight / 2 - this.textContainer.height / 2
+    );
+    this.addChild(this.textContainer);
+
+    // Reposition the text to the center of the window.
+    const center = new PIXI.Point(window.innerWidth / 2, window.innerHeight / 2);
+    const scoreContainerCenter = this.scoreContainer.toLocal(center);
+    this.scoreContainer.position.x = scoreContainerCenter.x - (this.scoreContainer.width / 2);
   }
 }
 
@@ -517,6 +560,8 @@ class FieldOverlay extends PIXI.Sprite {
       area.endFill();
 
       const moveNextBallToField = () => {
+        if (state.stage === GameStages.CONFIRMED) { return; }
+
         const nextBall = balls.find(ball => ball.selectedTarget === null);
         if (nextBall !== undefined) {
           makePrediction(state, area, nextBall);
@@ -995,9 +1040,9 @@ function initPredictionCorrectOverlayEvents(overlay, scoreTextPosition) {
     PIXI.actionManager.runAction(overlay.ball, ballMoveTo);
     PIXI.actionManager.runAction(overlay.ball, ballScaleTo);
     PIXI.actionManager.runAction(overlay.ball, ballFadeOut);
-    PIXI.actionManager.runAction(overlay.text, ballMoveTo);
-    PIXI.actionManager.runAction(overlay.text, ballScaleTo);
-    PIXI.actionManager.runAction(overlay.text, ballFadeOut);
+    PIXI.actionManager.runAction(overlay.textContainer, ballMoveTo);
+    PIXI.actionManager.runAction(overlay.textContainer, ballScaleTo);
+    PIXI.actionManager.runAction(overlay.textContainer, ballFadeOut);
     PIXI.actionManager.runAction(overlay.background, backgroundSequence);
   });
 }
@@ -1456,7 +1501,7 @@ configureRenderer(renderer);
 configureWebSocket(connection);
 
 // Load the fonts and sprites we need.
-configureFonts(['proxima-nova', 'proxima-nova-excn', 'SCOREBOARD'])
+configureFonts(['proxima-nova', 'proxima-nova-excn', 'SCOREBOARD', 'rockwell'])
   .then(() => {
     PIXI.loader
       .add('resources/prediction.json')
